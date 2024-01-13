09631ce0ae2d7e8121d0d549bc0a7f421f6b225f
Moises Alvarenga <moises.alvarenga@outlook.com>
2024-01-13 15:01:26 -0300

    Escrevendo testes para o metodo GetRandomNumberTrivia

--- test/features/number_trivia/domain/usercases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usercases/get_random_number_trivia_test.dart
@@ -1,7 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_tdd/core/usecases/usecase.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:flutter_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,30 +10,30 @@ import 'get_concrete_number_trivia_test.mocks.dart';
 
 @GenerateMocks([NumberTriviaRepository])
 void main() {
-  late GetConcreteNumberTrivia usecase;
+  late GetRandomNumberTrivia usecase;
   late MockNumberTriviaRepository mockNumberTriviaRepository;
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
 
   test(
-    "should get trivia for the number from the repository",
+    "should get trivia from the repository",
     () async {
       // arrange
-      const tNumber = 1;
+
       final tNumberTrivia = NumberTrivia(number: 1, text: 'test');
       // act
-      when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+      when(mockNumberTriviaRepository.getRandomNumberTrivia())
           .thenAnswer((_) async => Right(tNumberTrivia));
-      final result = await usecase.call(Params(number: tNumber));
+      final result = await usecase(NoParams());
 
       // assets
       //expect(result.isLeft(), true);
       expect(result.fold(id, id), equals(tNumberTrivia));
       expect(result.fold(id, id), isA<NumberTrivia>());
       expect(result, Right(tNumberTrivia));
-      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      verify(mockNumberTriviaRepository.getRandomNumberTrivia());
       verifyNoMoreInteractions(mockNumberTriviaRepository);
     },
   );
